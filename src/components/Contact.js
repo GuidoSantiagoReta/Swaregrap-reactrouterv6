816f35d64cafb18ae67b05759219ca9c6efc5945
Guido Santiago Reta <guidoreta@hotmail.com>
2023-02-05 20:25:49 -0300

    contact-section-2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,22 +9,22 @@ const Contact = () => {
  <Form className='container-form center-block' >
 
 <Form.Group className="mb-3 " controlId="form-name">
-<Form.Label>Name</Form.Label>
-<Form.Control style={{ width: "150px", margin:"auto" }} type="text" placeholder="Enter your name" />
+<Form.Label>Nombre</Form.Label>
+<Form.Control style={{ width: "150px", margin:"auto" }} type="text" placeholder="Nombre" />
 </Form.Group>
 
 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-  <Form.Label>Email address</Form.Label>
+  <Form.Label>Email</Form.Label>
   <Form.Control style={{ width: "150px", margin:"auto" }}  type="email" placeholder="name@example.com" />
 </Form.Group>
  
  <Form.Group  className="mb-3"controlId="form-phone">
-  <Form.Label>Phone number</Form.Label>
-  <Form.Control style={{ width: "150px", margin:"auto" }}   type="tel" placeholder="Enter your phone number" />
+  <Form.Label>Telefono</Form.Label>
+  <Form.Control style={{ width: "150px", margin:"auto" }}   type="tel" placeholder="Celular" />
 </Form.Group>
 
 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-  <Form.Label>Example textarea</Form.Label>
+  <Form.Label>Mensaje</Form.Label>
   <Form.Control style={{ width: "190px", margin:"auto" }}  as="textarea" rows={3} />
 </Form.Group>
 <Button variant="primary">Enviar</Button>
