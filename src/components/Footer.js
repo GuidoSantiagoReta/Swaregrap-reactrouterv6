817edd0f7fd31d4a494a23d8e1fdc690fdb124ca
Guido Santiago Reta <guidoreta@hotmail.com>
2023-02-08 18:28:50 -0300

    General modifications-5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-footer text-center text-lg-left">
+    <div className="bg-footer text-center text-lg-left ">
       <div className="text-center p-3 text-light">
         &copy; {new Date().getFullYear()} Copyright:{" "}
         <a
