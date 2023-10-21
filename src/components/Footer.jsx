a36e72f2c2acee172578758416c98dd9e6832df1
GuidoSantiagoReta <guidoreta@hotmail.com>
2023-10-21 18:52:27 -0300

    General-Modifications-27

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Footer = () => {
+  console.log("Rendering Home");
   return (
     <div className="bg-footer text-center text-lg-left ">
       <div className="text-center p-3 text-light">
