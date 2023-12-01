de5b27b74206230778867ff99138c0250a84409f
GuidoSantiagoReta <guidoreta@hotmail.com>
2023-12-01 18:42:04 -0300

    General-modifications-37

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   console.log("Rendering Home");
   return (
     <div className="bg-footer text-center text-lg-left ">
-      <div className="text-center p-3 text-light">
+      <div className="text-center p-3 text-light-foo">
         &copy; {new Date().getFullYear()} {"SwareGrap."}
         
       </div>
