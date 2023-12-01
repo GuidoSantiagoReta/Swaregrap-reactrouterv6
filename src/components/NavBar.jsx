bf41433e9a957f5a7551f8ff5865566f9751d2c5
GuidoSantiagoReta <guidoreta@hotmail.com>
2023-12-01 14:14:18 -0300

    General-modifications-33

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logosw from "../images/logosw.png";
+import '../App.css'
+
+
+
+
+const NavBar = () => {
+const [collapsed, setCollapsed] = useState(true);
+
+  return (
+    <>
+    <Navbar collapseOnSelect fixed="top" className=" animate__animated animate__fadeIn nav-bar" variant="dark" expand="md">
+    <Container fluid>
+      <Navbar.Brand as={Link} to="/home">
+        <img className="image-logo" src={logosw} id="inverse-rotation" height="35" alt="SwareGrap" />
+        SwareGrap
+      </Navbar.Brand>
+      <Navbar.Toggle
+        className="toggle"
+        aria-controls="basic-navbar-nav"
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {collapsed ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            height="2rem"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            height="2rem"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        )}
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto text-center">
+          <Nav.Link href="#home" as={Link} to="/">
+            Home
+          </Nav.Link>
+      
+          <Nav.Link href="#about" as={Link} to="/about">
+            Nosotros
+          </Nav.Link>
+          
+          <Nav.Link href="services" as={Link} to="/services">
+            Servicios
+          </Nav.Link>
+
+          <Nav.Link href="contact" as={Link} to="/contact">
+            Contacto
+          </Nav.Link>
+
+        </Nav>
+      </Navbar.Collapse>
+      
+    </Container>
+  </Navbar>
+  </>
+  )
+}
+
+export default NavBar;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,83 +1,21 @@
-import React, { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+
+import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Slider from "../components/slider/Slider";
-import logosw from "../images/logosw.png";
+import NavBar from "../components/NavBar";
+
+
+
 
 const MainLayaout = () => {
-  const [collapsed, setCollapsed] = useState(true);
+
 
   return (
     <>
-    {/*NavBar*/}
-      <Navbar collapseOnSelect fixed="top" className=" animate__animated animate__fadeIn nav-bar" variant="dark" expand="md">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/home">
-            <img className="image-logo" src={logosw} id="inverse-rotation" height="35" alt="SwareGrap" />
-            SwareGrap
-          </Navbar.Brand>
-          <Navbar.Toggle
-            className="toggle"
-            aria-controls="basic-navbar-nav"
-            onClick={() => setCollapsed(!collapsed)}
-          >
-            {collapsed ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                height="2rem"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                height="2rem"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            )}
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto text-center">
-              <Nav.Link href="#home" as={Link} to="/">
-                Home
-              </Nav.Link>
-          
-              <Nav.Link href="#about" as={Link} to="/about">
-                Nosotros
-              </Nav.Link>
-              
-              <Nav.Link href="services" as={Link} to="/services">
-                Servicios
-              </Nav.Link>
 
-              <Nav.Link href="contact" as={Link} to="/contact">
-                Contacto
-              </Nav.Link>
+      <NavBar/>
 
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      {/*mostrar el contenido de la vista -representa a los hijos rutas */}
       <section>
         <Outlet></Outlet>   
       </section>
